4dde24c070c816aac7afc1dcb3e3d4feac9c54a5
DieserMastro <s0588238@htw-berlin.de>
2025-10-23 16:30:37 +0200

    ??

---
+++ Assets/Scripts/Border_Controller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Border_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private Transform target;
+    void Update()
+    { 
+        transform.position = new Vector3(offset.x, target.position.y, offset.z);
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.rigidbody != null)
+        {
+            Vector2 linearVelocity = collision.rigidbody.linearVelocity;
+            Debug.Log(collision);
+            linearVelocity.x = -linearVelocity.x;
+            collision.rigidbody.linearVelocity = linearVelocity;
+        }
+    }
+}
